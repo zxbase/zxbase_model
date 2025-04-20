fe2529df453b8f2bfcdaf6fe66ef9003a816b19c
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-04-19 18:31:11 -0700

    remove library name

--- lib/zxbase_model.dart
+++ lib/zxbase_model.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library zxbase_model;
+library;
 
 export 'src/identity.dart';
 export 'src/token.dart';
